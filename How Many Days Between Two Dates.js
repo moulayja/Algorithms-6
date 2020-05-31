60cdf327dff9461a202b34c7a636805cf0de598d
Jamal <moulayjam@gmail.com>
2020-05-31 11:26:29 -0400

    How Many Days Between Two Dates

---
+++ How Many Days Between Two Dates.js
@@ -0,0 +1,5 @@
+// Create a function that takes two dates and returns the number of days between the first and second date.
+
+function getDays(date1, date2) {
+    return Math.ceil((date2 - date1) / 8.64e7);
+  }
